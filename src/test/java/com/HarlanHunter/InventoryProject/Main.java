2d57f10352f108f08fe36340baa4ae43d9f0bcef
Hunter Shaw <huntershaw0@gmail.com>
2021-03-30 22:23:12 -0400

    MAIN LOOP LAYOUT
    Added the basic outline for how the main part of the program will work
    and the interactions between the user and the program should go.

--- src/test/java/com/HarlanHunter/InventoryProject/Main.java
+++ src/test/java/com/HarlanHunter/InventoryProject/Main.java
@@ -3,6 +3,7 @@ package src.test.java.com.HarlanHunter.InventoryProject;
 public class Main {
 
 	public static void main(String[] args) {
+		/*		########	TESTING COLLECTIONS 	########
 		List<String> lst = new AList<>();
 		Dictionary<String, Integer> dict = new HashDictionary<>();
 		lst.add("Mike");
@@ -13,6 +14,30 @@ public class Main {
 		dict.put("LaVa", 64);
 		System.out.println(dict.size());
 		System.out.println(lst.size());
+		*/
+		
+		/*
+		 * Main loop of program should check always before anything if there is a data file. This data file is where the products
+		 * within the collections will be stored. If the program checks and there is no data file create a new one, if there is a data file 
+		 * and it contains data load the collections with the data. Again, this will be done before any interactions with the actual user. 
+		 * 
+		 * Once we get or create the data file we will ask to input data if the data file was just created as there is no data. However, if the data
+		 * file does exist then we will display a current list of the products. There will be commands to navigate the list of products. 
+		 * 
+		 * COMMANDS: How the user will interact with the data inside of the collections. 
+		 *  - ADD
+		 *  - REMOVE
+		 *  - SEARCH
+		 *  
+		 * The ADD command will be how the user can add an element(product) to the collections and when an ADD is successful, display the updated list. 
+		 * 
+		 * The REMOVE command is used to remove a specific element(product) from the collections and if successful, the newly updated list is automatically displayed. 
+		 * 
+		 * The SEARCH command will used to search for a unique product in the list, if the search is successful display that single product to the user. Prompt them 
+		 * to go back to the list and wait for a response. 
+		 */
+		
+		
 	}
 
 }
