11915addd565504017bca49e88a07bc5e501e06a
Hunter Shaw <huntershaw0@gmail.com>
2021-04-10 22:45:08 -0400

    sdjk

--- src/test/java/com/HarlanHunter/InventoryProject/AStack.java
+++ src/test/java/com/HarlanHunter/InventoryProject/AStack.java
@@ -2,12 +2,22 @@ package src.test.java.com.HarlanHunter.InventoryProject;
 
 import java.util.EmptyStackException;
 
+/**
+ * Array based implementation of Stack interface.
+ * @author Hunter Shaw
+ *
+ * @param <E>
+ */
 public class AStack<E> implements Stack<E> {
 	
 	private Object[] ar;
 	private int size;
 	private int capacity;
 	final private int MAX_CAPACITY = 10;
+	
+	
+	
+	//########### CONSTRUCTORS ###########
 	
 	public AStack() {
 		capacity = MAX_CAPACITY;
@@ -20,6 +30,8 @@ public class AStack<E> implements Stack<E> {
 		ar = new Object[capacity];
 		size = 0;
 	}
+	
+	
 	
 	@Override
 	public boolean empty() {
