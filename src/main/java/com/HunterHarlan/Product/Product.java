bf8ffb52fde197da62167446e06434a3acb66936
Hunter Shaw <huntershaw0@gmail.com>
2021-06-14 14:32:56 -0400

    Added Version and author to Product class Finished 2 TODOs in LinkedHashMap class and added class documentation

--- src/main/java/com/HunterHarlan/Product/Product.java
+++ src/main/java/com/HunterHarlan/Product/Product.java
@@ -4,6 +4,8 @@ import java.io.Serializable;
 
 /**
  * Class which will define a item in the system as a product.
+ * @author Hunter Shaw
+ * @version 0.1
  */
 public class Product implements Serializable, Item {
     private String name;
