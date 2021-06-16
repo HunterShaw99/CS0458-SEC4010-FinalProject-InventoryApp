2059b5bfa9c5cdcb3c03a6b4a1e60613824e6acd
Hunter Shaw <huntershaw0@gmail.com>
2021-06-16 13:30:41 -0400

    CHanged the name of the Hash class from HashFunction to Hashing

--- src/main/java/com/HunterHarlan/HashFunction/HashFunction.java
+++
@@ -1,5 +0,0 @@
-package com.HunterHarlan.HashFunction;
-
-public class HashFunction {
-
-}
---
+++ src/main/java/com/HunterHarlan/HashFunction/Hashing.java
@@ -0,0 +1,10 @@
+package com.HunterHarlan.HashFunction;
+
+/**
+ * Class used for hashing the key in a key-value pair. Contains all the functions involving hashing.
+ * @author Hunter Shaw
+ * @version 0.1
+ */
+public class Hashing {
+
+}
